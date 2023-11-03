ecbdaef4ac62a6c5c86128bfae1994c01881e9f7
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-11-03 21:05:57 +0200

    03 NOV 23

---
+++ 08. Inheritance - Exercise/05. Shop/project/product_repository.py
@@ -0,0 +1,22 @@
+from project.product import Product
+from typing import List, Optional
+
+class ProductRepository:
+    def __init__(self):
+        self.products: List[Product] = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str) -> Optional[Product]:
+        for p in self.products:
+            if p.name == product_name:
+                return p
+
+    def remove(self, product_name: str):
+        product = self.find(product_name)
+        if product is not None:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return "\n".join([f'{p.name}: {p.quantity}'for p in self.products])
