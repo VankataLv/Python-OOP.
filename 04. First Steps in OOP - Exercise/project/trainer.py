1b1947548b8374de73cfe4640160b953f24f6678
VankataLv <132002763+VankataLv@users.noreply.github.com>
2023-11-21 11:43:36 +0200

    21 NOV 23

--- 04. First Steps in OOP - Exercise/project/trainer.py
+++ 04. First Steps in OOP - Exercise/project/trainer.py
@@ -1,5 +1,5 @@
 from typing import List
-from pokemon import Pokemon
+from project.pokemon import Pokemon
 
 
 class Trainer:
@@ -15,8 +15,32 @@ class Trainer:
             return f"This pokemon is already caught"
 
     def release_pokemon(self, pokemon_name: str):
-        if pokemon_name in self.pokemons:
-            self.pokemons.remove(pokemon_name)
-            return"You have released {pokemon_name}"
+        searched_pokemon = self.find_pokemon_by_name(pokemon_name)
+        if searched_pokemon:
+            self.pokemons.remove(searched_pokemon)
+            return f"You have released {pokemon_name}"
         else:
             return "Pokemon is not caught"
+
+    def find_pokemon_by_name(self, given_name):
+        for pokemon in self.pokemons:
+            if pokemon.name == given_name:
+                return pokemon
+
+    def trainer_data(self):
+        pok_data = "\n".join([f"- {x.pokemon_details()}" for x in self.pokemons])
+        return f"""Pokemon Trainer {self.name}
+Pokemon count {len(self.pokemons)}
+{pok_data}"""
+
+# pokemon = Pokemon("Pikachu", 90)
+# print(pokemon.pokemon_details())
+# trainer = Trainer("Ash")
+# print(trainer.add_pokemon(pokemon))
+# second_pokemon = Pokemon("Charizard", 110)
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.add_pokemon(Pokemon("TEST_P", 99)))
+# print(trainer.trainer_data())
